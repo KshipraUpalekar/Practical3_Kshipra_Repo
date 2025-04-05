4a8a93cbee31770a88c28950ec68b928b4b74561
KshipraUpalekar <kshipraupalekar2002@gmail.com>
2025-04-05 19:26:58 +0530

    Update HelloText.java

--- HelloText.java
+++ HelloText.java
@@ -1,1 +1,14 @@
-
+public class HelloTest { 
+  public static void main(String[] args) { 
+    if (Hello.isPositive(5)) { 
+        System.out.println("Test passed: 5 is positive"); 
+      } else { 
+        System.out.println("Test failed!"); 
+      } 
+      if (!Hello.isPositive(-3)) { 
+        System.out.println("Test passed: 3 is not positive"); 
+      } else { 
+        System.out.println("Test failed!"); 
+      } 
+    } 
+}
