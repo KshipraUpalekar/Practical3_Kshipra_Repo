d7b89ffc7c3f2696e57ca260c99b936d84266782
KshipraUpalekar <kshipraupalekar2002@gmail.com>
2025-04-05 19:29:26 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello { 
+  public static void main(String[] args) { 
+    System.out.println("Hello from GitLab CI/CD!"); 
+  } 
+  // Simple method to "test" 
+  public static boolean isPositive (int number) { 
+    return number > 0; 
+  } 
+}
